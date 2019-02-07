cb9a629a4f24bf4c8165f2284d05e9095fb873cb
양성호 <ysh8272002@naver.com>
2019-02-07 13:45:43 +0900

    Add files via upload

--- bluebaikal/settings.py
+++ bluebaikal/settings.py
@@ -43,10 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'bkio',
-	"sslserver",
+	'bkio'
 ]
-
+#"sslserver",
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
